d5b6de075c329828526f6fa680acba62b3f8e9f2
Ishay Mamluk <ishay.mamluk@clinch.co>
2025-03-28 13:06:25 +0300

    add control for arms

--- control_with_xbox.py
+++ control_with_xbox.py
@@ -7,6 +7,8 @@ from pybricks.tools import wait
 hub = PrimeHub()
 motor_A = Motor(Port.A, Direction.CLOCKWISE)
 motor_B = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+motor_C = Motor(Port.C)
+motor_D = Motor(Port.D)
 controller = XboxController()
 
 # תקרת מהירות
@@ -17,6 +19,7 @@ while True:
 
     # קבלת ערכי הג'ויסטיק השמאלי
     x, y = controller.joystick_left()
+    right_x, right_y = controller.joystick_right()
 
     # חישוב מהירות לכל מנוע לפי נוסחת טנק
     right = y + x / 3
@@ -26,8 +29,24 @@ while True:
     motor_A.run(left * 10.5)
     motor_B.run(right * 10.5)
 
-    # סיום בלחיצת RB
-    if Button.RB in buttons:
+    # שליטה במנוע C עם RT (קדימה) ו-RB (אחורה)
+    if right_y > 0:
+        motor_C.run(right_y * MAX_SPEED)
+    elif right_y < 0:
+        motor_C.run(-MAX_SPEED)
+    else:
+        motor_C.stop()
+
+    # שליטה במנוע D עם LT (קדימה) ו-LB (אחורה)
+    if Button.RB > 0:
+        motor_D.run(MAX_SPEED)
+    elif Button.LB in buttons:
+        motor_D.run(-MAX_SPEED)
+    else:
+        motor_D.stop()
+
+    # סיום בלחיצת כפתור התפריט
+    if Button.MENU in buttons:
         hub.speaker.beep()
         print("Goodbye!")
         break
