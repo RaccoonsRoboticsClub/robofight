865ee8f9eeb59eb05e047c5fb5b7c533004cedde
RaccoonsRoboticsClub <168541544+RaccoonsRoboticsClub@users.noreply.github.com>
2025-04-01 20:09:23 +0300

    Update control_with_xbox.py

--- control_with_xbox.py
+++ control_with_xbox.py
@@ -5,31 +5,47 @@ from pybricks.parameters import Port, Direction, Stop, Button
 from pybricks.tools import wait
 
 hub = PrimeHub()
-motor_A = Motor(Port.A, Direction.CLOCKWISE)
-motor_B = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+motor_A = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+motor_B = Motor(Port.B, Direction.CLOCKWISE)
+motor_C = Motor(Port.C, Direction.CLOCKWISE)
+motor_D = Motor(Port.D, Direction.CLOCKWISE)
+
 controller = XboxController()
 
 # תקרת מהירות
 MAX_SPEED = 1050
+speed_factor = MAX_SPEED / 10 # 100% speed == MAX_SPEED
 
 while True:
-    buttons = controller.buttons.pressed()
-
-    # קבלת ערכי הג'ויסטיק השמאלי
+    pressed_buttons = controller.buttons.pressed()
     x, y = controller.joystick_left()
+    lt, rt = controller.triggers()
 
-    # חישוב מהירות לכל מנוע לפי נוסחת טנק
-    right = y + x / 3
-    left = y - x / 3
+    lt_pressed = lt > 0
+    rt_pressed = rt > 0
+    rb_pressed = Button.RB in pressed_buttons
+    lb_pressed = Button.LB in pressed_buttons
 
-    # קנה מידה למהירות המקסימלית
-    motor_A.run(left * 10.5)
-    motor_B.run(right * 10.5)
+    right = y - x / 3
+    left = y + x / 3
 
-    # סיום בלחיצת RB
-    if Button.RB in buttons:
-        hub.speaker.beep()
-        print("Goodbye!")
-        break
+    motor_A.run(left * speed_factor)
+    motor_B.run(right * speed_factor)
+
+    # Motor D
+    if lt_pressed:
+        motor_D.run(MAX_SPEED)
+    elif lb_pressed:
+        motor_D.run(-MAX_SPEED)
+    else:
+        motor_D.stop()
+
+    # Motor C
+    if rt_pressed:
+        motor_C.run(MAX_SPEED)
+    elif rb_pressed:
+        motor_C.run(-MAX_SPEED)
+    else:
+        motor_C.stop()
 
     wait(20)
